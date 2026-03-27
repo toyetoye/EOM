c6265f8205ac385c8aadf8b273f3ca8b190ea1ab
toyetoye <38019569+toyetoye@users.noreply.github.com>
2026-03-27 16:22:32 +0100

    fix: import pool from db.js for migration endpoint

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { initDB, addUMSTables } = require('./db');
+const { initDB, addUMSTables, pool } = require('./db');
 
 const app = express();
 
